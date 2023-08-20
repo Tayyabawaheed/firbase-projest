34bfecce83acd356962466bd77e28bba104907cd
Tayyabawaheed <124914725+Tayyabawaheed@users.noreply.github.com>
2023-08-20 12:35:08 +0500

    Add files via upload

---
+++ fb-configue.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ0PLObPz400ECz7c71lBVSV6MzE4-hHw",
+    authDomain: "smit-application.firebaseapp.com",
+    databaseURL: "https://smit-application-default-rtdb.firebaseio.com",
+    projectId: "smit-application",
+    storageBucket: "smit-application.appspot.com",
+    messagingSenderId: "28503545348",
+    appId: "1:28503545348:web:7c244e40b9cfa369eac751",
+    measurementId: "G-Y0K4XPPE13"
+  };
